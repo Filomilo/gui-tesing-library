6f68cdb558d8302475361837fe7304fabc2489b1
Filomilo <fborowiec@wp.pl>
2025-03-26 17:34:46 +0100

    added dotnet resotre

--- src/runTest.bat
+++ src/runTest.bat
@@ -3,5 +3,6 @@ cd C:\OEM\GuiTestingLibrary_Tests
 @REM "C:\Program Files\dotnet\dotnet.exe" test >> \\host.lan\Data\\dotnet-output.log
 @REM "C:\Program Files\dotnet\dotnet.exe" test 
 @REM "C:\Program Files\dotnet\dotnet.exe" test >> \\host.lan\Data\\dotnet-output.log
+"C:\Program Files\dotnet\dotnet.exe" dotnet restore >> \\host.lan\Data\\dotnet.log
 "C:\Program Files\dotnet\dotnet.exe" dotnet test --logger "trx;LogFileName=\\host.lan\Data\\test-results.xml" >> \\host.lan\Data\\test.log
 echo "fin" >> \\host.lan\Data\\fin
