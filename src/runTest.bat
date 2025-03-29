f23b96e4f33869941b21003194de4d813982461e
Filomilo <fborowiec@wp.pl>
2025-03-29 13:45:02 +0100

    changed file format to trx

--- src/runTest.bat
+++ src/runTest.bat
@@ -8,6 +8,6 @@ cd C:\OEM\GuiTestingLibrary_Tests
 @REM "C:\Program Files\dotnet\dotnet.exe" dotnet add package Lombok.NET --version 2.4.1 >> \\host.lan\Data\\dotnet.log
 @REM "C:\Program Files\dotnet\dotnet.exe" dotnet add package System.Numerics.Vectors --version 4.1.6  >> \\host.lan\Data\\dotnet.log
 
-"C:\Program Files\dotnet\dotnet.exe" dotnet test --logger "trx;LogFileName=\\host.lan\Data\\test-results.xml" >> \\host.lan\Data\\test.log
+"C:\Program Files\dotnet\dotnet.exe" dotnet test --logger "trx;LogFileName=\\host.lan\Data\\test-results.trx" >> \\host.lan\Data\\test.log
 powershell -Command "(Get-Content \\host.lan\Data\\test-results.xml) -join \"`n\" | Set-Content \\host.lan\Data\\test-results.xml -NoNewline"
 echo "fin" >> \\host.lan\Data\\fin
