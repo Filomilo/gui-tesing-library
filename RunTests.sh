51e1ced05cd3c884463927d3f88b0c8d0e593dbc
Filomilo <fborowiec@wp.pl>
2025-03-31 19:49:41 +0200

    decrest run test timout

--- RunTests.sh
+++ RunTests.sh
@@ -20,13 +20,13 @@ mkdir shared
 echo "starting docker"
 docker compose up -d
 
-TIMEOUT=$((60 * 60 * 2))
+TIMEOUT=$((60 * 60))
 SECONDS=0
 
 echo "Waiting for docker test to finish"
 until [ -f "shared/fin" ] || (( SECONDS >= TIMEOUT )); do sleep 1;  done
 
 
-# docker compose down
+docker compose down
 
 
