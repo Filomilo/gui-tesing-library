f6f9ec8abe828748caa42ee795b0fde71424d428
Filomilo <fborowiec@wp.pl>
2025-04-18 09:03:07 +0200

    chocko upgrade

--- src/install.bat
+++ src/install.bat
@@ -2,6 +2,7 @@ timeout 100
 taskkill /f /im explorer.exe
 winget install ffmpeg --accept-source-agreements --accept-package-agreements >> \\host.lan\Data\\winget-output.log
 winget install Chocolatey --accept-source-agreements --accept-package-agreements >> \\host.lan\Data\\winget-output.log
+choco upgrade chocolatey  --accept-package-agreements >> \\host.lan\Data\\chocko-output.log
 winget install Oracle.JDK.21 --accept-source-agreements --accept-package-agreements >> \\host.lan\Data\\winget-output.log
 winget install Microsoft.DotNet.SDK.9 --accept-source-agreements --accept-package-agreements >> \\host.lan\Data\\winget-output.log
 winget install Microsoft.DotNet.Framework.DeveloperPack_4 -v 4.7.2 --accept-source-agreements --accept-package-agreements >> \\host.lan\Data\\winget-output.log
