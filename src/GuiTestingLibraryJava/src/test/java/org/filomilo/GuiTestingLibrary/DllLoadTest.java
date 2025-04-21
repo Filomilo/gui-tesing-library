629e9ad89c13bf7a81aac38501190b1b242cc1bb
Filomilo <fborowiec@wp.pl>
2025-04-21 21:11:00 +0200

    created java libaryr proejct

---
+++ src/GuiTestingLibraryJava/src/test/java/org/filomilo/GuiTestingLibrary/DllLoadTest.java
@@ -0,0 +1,18 @@
+package org.filomilo.GuiTestingLibrary;
+
+
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.Test;
+
+
+@Log
+class DllLoadTest {
+
+    @Test
+    public void LoadDll() {
+        log.info("test");
+    }
+
+
+}
+
